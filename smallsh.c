96c9d77427530d4d6f5268198d6085030f52a65c
efmmoncada <emmanuel.moncada27@gmail.com>
2021-11-04 20:15:49 -0700

    160/170 working, reporting signals broken

--- smallsh.c
+++ smallsh.c
@@ -6,21 +6,39 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 
+// global vars to track status of last process and child prcesses.
 int status = 0;
 pid_t proc_arr[200] = {0};
 int num_proc = 0;
 
+// global var to use wtih SIGTSTP
+int foreground_only = 0;
+
+#define TRASH "/dev/null"
+#define PROMPT ": "
+
 #include "smallsh.h"
 
-#define PROMPT ":"
-
-// still have to do signal handleres
-
 int main(int argc, char const *argv[]) {
+  // signal handlers fof parent
+  struct sigaction SIGINT_action = {0};
+  SIGINT_action.sa_handler = SIG_IGN;
+  sigfillset(&SIGINT_action.sa_mask);
+  SIGINT_action.sa_flags = SA_RESTART;
+  sigaction(SIGINT, &SIGINT_action, NULL);
+
+
+  struct sigaction SIGTSTP_action = {0};
+  SIGTSTP_action.sa_handler = handle_SIGTSTP;
+  sigfillset(&SIGTSTP_action.sa_mask);
+  SIGTSTP_action.sa_flags = SA_RESTART;
+  sigaction(SIGTSTP, &SIGTSTP_action, NULL);
+
 
   char input[2049] = {0};
-
 
   while (1) {
 
@@ -32,6 +50,13 @@ int main(int argc, char const *argv[]) {
       char *token = strtok(input, "\n");
       if (strcmp(token, "exit") == 0) {
         // clean up and kill processes created, then exit;
+        if (num_proc != 0) {
+          for (int i = 0; i < num_proc; i++) {
+            kill(proc_arr[i], SIGKILL);
+          }
+          // clean mem allocd
+          //...
+        }
         return 0;
       } else {
         char *args[512] = {0};
@@ -39,5 +64,4 @@ int main(int argc, char const *argv[]) {
       }
     }
   }
-
 }
--- smallsh.h
+++ smallsh.h
@@ -1,7 +1,20 @@
+void handle_SIGTSTP(int signo) {
+  if (foreground_only == 0) {
+    char *msg = "\nEntering foreground only mode\n";
+    write(STDOUT_FILENO, msg, 31);
+    foreground_only = 1;
+  }
+  else {
+    char *msg = "\nExiting foreground only mode\n";
+    write(STDOUT_FILENO, msg, 30);
+    foreground_only = 0;
+  }
+}
+
 char* expand(char* str) {
   char pid[16] = {0};
   sprintf(pid, "%d", getpid());
-
+  // find $$ location
   char *front = calloc(strlen(str) + 1, sizeof(char));
   char *back = calloc(strlen(str) + 1, sizeof(char));
   int i, j;
@@ -11,10 +24,11 @@ char* expand(char* str) {
       break;
     }
   }
+  //split string
   strncpy(front, str, i);
   strncpy(back, str + j, strlen(str) - j);
   char *final = calloc(strlen(front) + strlen(pid) + strlen(back), sizeof(char));
-  sprintf(final, "%s%s%s", front, pid, back);
+  sprintf(final, "%s%s%s", front, pid, back);       // concat
   return final;
 }
 
@@ -25,10 +39,37 @@ void change_dir(char **args) {
 
 void external_fg(char **args, char *in_file, char *out_file) {
   pid_t pid = fork();
-  if (pid == 0) {
-    // only runs in child proccess
+  if (pid == 0) {         // only runs in child proccess
+    // redirect i/o
+    if (in_file != NULL) {
+      int fd = open(in_file, O_RDONLY);
+      if (fd == -1) {
+        printf("Could not open file %s for redirection.\n", in_file);
+        exit(1);
+      }
+      dup2(fd, STDIN_FILENO);
+    }
+    if (out_file != NULL) {
+      int fd = open(out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+      if (fd == -1) {
+        printf("Could not open file %s for redirection.\n", out_file);
+        exit(1);
+      }
+      dup2(fd, STDOUT_FILENO);
+    }
 
-    // redirect input / output using dup2
+    // modify sig handlers for child processes.
+    struct sigaction SIGINT_action = {0};
+    SIGINT_action.sa_handler = NULL;
+    sigfillset(&SIGINT_action.sa_mask);
+    SIGINT_action.sa_flags = SA_RESTART;
+    sigaction(SIGINT, &SIGINT_action, NULL);
+
+    struct sigaction SIGTSTP_action = {0};
+    SIGTSTP_action.sa_handler = SIG_IGN;
+    sigfillset(&SIGTSTP_action.sa_mask);
+    SIGTSTP_action.sa_flags = SA_RESTART;
+    sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
     execvp(args[0], args);
     perror("execv");   /* execve() returns only on error */
@@ -36,10 +77,22 @@ void external_fg(char **args, char *in_file, char *out_file) {
   }
   else {
     waitpid(pid, &status, 0);
+    if (WIFSIGNALED(status)) {
+      printf("\nProcess killed by signal %d\n", WTERMSIG(status));
+    }
   }
 }
 
 void external_bg(char **args, char *in_file, char *out_file) {
+  if (in_file == NULL) {
+    in_file = calloc(strlen(TRASH) + 1, sizeof(char));
+    strcpy(in_file, TRASH);
+  }
+  if (out_file == NULL) {
+    out_file = calloc(strlen(TRASH) + 1, sizeof(char));
+    strcpy(out_file, TRASH);
+  }
+
   pid_t child_pid = fork();
   if (child_pid == -1) {
     perror("fork() failed!");
@@ -47,7 +100,29 @@ void external_bg(char **args, char *in_file, char *out_file) {
   }
   else if (child_pid == 0) {
     // only runis in child
-    // redirect i/o using dup(2), if not specified, redirect to dev/null
+    // redirect i/o
+    if (in_file != NULL) {
+      int fd = open(in_file, O_RDONLY);
+      if (fd == -1) {
+        printf("Could not open file %s for redirection.\n", in_file);
+        exit(1);
+      }
+      dup2(fd, STDIN_FILENO);
+    }
+    if (out_file != NULL) {
+      int fd = open(out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+      if (fd == -1) {
+        printf("Could not open file %s for redirection.\n", out_file);
+        exit(1);
+      }
+      dup2(fd, STDOUT_FILENO);
+    }
+    // reset signal handler for background child
+    struct sigaction SIGTSTP_action = {0};
+    SIGTSTP_action.sa_handler = handle_SIGTSTP;
+    sigfillset(&SIGTSTP_action.sa_mask);
+    SIGTSTP_action.sa_flags = SA_RESTART;
+    sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
     execvp(args[0], args);
     perror("execvp failed!");
@@ -64,16 +139,18 @@ void external_bg(char **args, char *in_file, char *out_file) {
 
 
 void handle_command(char** args, int num_args, char* in_file, char* out_file) {
+  // check for built in commands
   if (strcmp(args[0], "\n") == 0 || args[0][0] == '#') return;
   if (strcmp(args[0], "cd") == 0) change_dir(args);
   else if (strcmp(args[0], "status") == 0) {
     if (WIFEXITED(status)) printf("Last foreground process exited normally with status %d\n", WEXITSTATUS(status));
     else printf("Last foreground process exited abnormally due to signal %d\n", WTERMSIG(status));
-  }
+  } // check if it should be run in background, but only if not in foreground only mode
   else if (strcmp(args[num_args - 1], "&") == 0) {
     free(args[num_args-1]);
     args[num_args-1] = NULL;
-    external_bg(args, in_file, out_file);
+    if (foreground_only == 1) external_fg(args, in_file, out_file);
+    else external_bg(args, in_file, out_file);
   }
   else {
     external_fg(args, in_file, out_file);
@@ -114,13 +191,13 @@ void tokenize_commands(char** arg_array, char* line) {
 
 void check_children_status(pid_t *child_array, int *num_children) {
   for (int i = 0; i < *num_children; i++) {
-    int *status;
-    if (waitpid(child_array[i], status, WNOHANG) != 0) {
-      if (WIFEXITED(status)) {
-        printf("background process %d exited normally with status %d\n", child_array[i], WEXITSTATUS(status));
+    int *child_stat;
+    if (waitpid(child_array[i], child_stat, WNOHANG) != 0) {
+      if (WIFEXITED(child_stat)) {
+        printf("background process %d exited normally with status %d\n", child_array[i], WEXITSTATUS(child_stat));
       }
       else {
-        printf("Background process %d exited abnormally due to signal %d\n", child_array[i], WTERMSIG(status));
+        printf("Background process %d exited abnormally due to signal %d\n", child_array[i], WTERMSIG(child_stat));
       }
       for (int j = i; j < *num_children; j++) {
         child_array[j] = child_array[j+1];
